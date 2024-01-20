c2d43c4efec56fcf5422082ba02bd1770b463864
pdressen <pdressen@protonmail.com>
2024-01-21 00:07:19 +0100

    Benchmark read/write coils / holding registers

--- esp32-modbus-gateway/src/main.cpp
+++ esp32-modbus-gateway/src/main.cpp
@@ -3,11 +3,22 @@
 #include <ModbusTCP.h>
 #include "config.h"
 
+// enable logging each request to serial
+//#define REQUEST_LOGGING
+
 IPAddress wifiIp = ipaddr_addr(WIFI_IP);
 IPAddress wifiSubnet = ipaddr_addr(WIFI_SUBNET);
 IPAddress wifiGateway = ipaddr_addr(WIFI_GATEWAY);
 
 ModbusTCP modbusTcp;
+
+
+#ifdef REQUEST_LOGGING
+Modbus::ResultCode modbusRequest(Modbus::FunctionCode fc, const Modbus::RequestData rd) {
+  Serial.printf("Modbus request FC: 0x%02x ADDR: %d CNT: %d\n", fc, rd.regMask.address, rd.regCount & rd.andMask);  
+  return Modbus::ResultCode::EX_PASSTHROUGH;
+}
+#endif
 
 void setup() {
   // put your setup code here, to run once:
@@ -17,6 +28,7 @@ void setup() {
   Serial.println();
 
   WiFi.config(wifiIp, wifiGateway, wifiSubnet);
+  WiFi.setAutoReconnect(true);
 
   while (WL_CONNECTED != WiFi.begin(WIFI_SSID, WIFI_PSK)) {
     Serial.printf("Connecting to WiFi \"%s\", status: %u\n", WIFI_SSID, WiFi.status());
@@ -24,12 +36,21 @@ void setup() {
   }
   Serial.printf("Connected. IP: \"%s\"...\n", WiFi.localIP().toString());
 
-  Serial.printf("Starting ModbusTCP server...");
+  Serial.println("Starting ModbusTCP server...");
+
+#ifdef REQUEST_LOGGING
+  modbusTcp.onRequestSuccess(modbusRequest);
+#endif
+
   modbusTcp.server(MODBUS_SERVER_PORT);
-  if (modbusTcp.addHreg(0, 0, MODBUS_NUM_HREGS))
-    Serial.println("Added Hregs");
+
+  if (modbusTcp.addCoil(0, false, MODBUS_NUM_REGS)
+      && modbusTcp.addIsts(0, false, MODBUS_NUM_REGS)
+      && modbusTcp.addIreg(0, 0, MODBUS_NUM_REGS)
+      && modbusTcp.addHreg(0, 0, MODBUS_NUM_REGS))
+    Serial.printf("Added %u registers of each kind\n", MODBUS_NUM_REGS);
   else
-    Serial.println("Couldn't add Hregs");
+    Serial.println("Couldn't add registers");
 }
 
 void loop() {
--- modbus-benchmark/app.py
+++ modbus-benchmark/app.py
@@ -4,15 +4,21 @@ from json import load
 from statistics import stdev
 
 # consts
+NUM_REGS = 96
 NUM_ITERATIONS = 1_000   # how many times to run a read/write cycle
+OUTPUT_VERBOSE_TIMING = True    # if each single cycle time should be logged
 
 # load config.json
 config = None
 with open("config.json", "r") as config_file:
     config = load(config_file)
+MODBUS_TCP_HOST = config["modbus_tcp"]["host"]
+MODBUS_TCP_PORT = config["modbus_tcp"]["port"]
+MODBUS_TCP_ID = config["modbus_tcp"]["id"]
 
 # create ModbusTCP client (master)
-c = ModbusClient(host=config["modbus_tcp"]["host"], port=config["modbus_tcp"]["port"], unit_id=config["modbus_tcp"]["id"], auto_open=True)
+print(f"Connecting to {MODBUS_TCP_HOST}:{MODBUS_TCP_PORT}, unit-ID {MODBUS_TCP_ID}...")
+c = ModbusClient(host=MODBUS_TCP_HOST, port=config["modbus_tcp"]["port"], unit_id=config["modbus_tcp"]["id"], auto_open=True)
 if c.open():
     # let everything settle for a moment
     print("Connection successful")
@@ -26,21 +32,40 @@ if c.open():
         start_time = perf_counter()
 
         # read
-        regs = c.read_holding_registers(0, 100)        
-        if not regs:
+        hregs = c.read_holding_registers(0, NUM_REGS)        
+        if not hregs:
+            raise Exception(c.last_error_as_txt)
+        coils = c.read_coils(0, NUM_REGS)        
+        if not coils:
             raise Exception(c.last_error_as_txt)
         
         # write
-        # if not c.write_multiple_registers(30000, regs):
-        #     raise Exception(c.last_error_as_txt)
+        if not c.write_multiple_registers(0, hregs):
+             raise Exception(c.last_error_as_txt)        
+        if not c.write_multiple_coils(0, coils):
+             raise Exception(c.last_error_as_txt)
         
         duration = perf_counter() - start_time
         durations[i] = duration
 
     
+    c.close()
+
     print(f"Done")
+    
+    if OUTPUT_VERBOSE_TIMING:
+        print("######## INDIVIDUAL CYCLE DURATIONS #########")
+        print("#cycle\tduration")
+        for i in range(len(durations)):
+            print(f"{i}\t{durations[i]:.4f}")
+
+    print("######## SUMMARY #########")
     print("#total\tmax\tmin\tavg\tstddev")
-    print(f"{sum(durations):.4f}\t{max(durations):.4f}\t{min(durations):.4f}\t{(sum(durations)/len(durations)):.4f}\t{stdev(durations):.4f}")
+    std_deviation = stdev(durations) if (len(durations) > 1)  else 0
+    print(f"{sum(durations):.4f}\t{max(durations):.4f}\t{min(durations):.4f}\t{(sum(durations)/len(durations)):.4f}\t{std_deviation:.4f}")
+
+    print("#################")
+    print("Note: All times in seconds")
         
 else:
     print(f"Error opening modbus connection: {c.last_error_as_txt}")
